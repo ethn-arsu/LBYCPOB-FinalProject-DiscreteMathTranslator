73c223d0f9d346bb83e2a14b0b301bebc0759e01
aeon.mauricio <aeon_mauricio@dlsu.edu.ph>
2026-08-10 14:06:32 +0800

    Created base for Main

--- src/main/java/ph/edu/dlsu/lbycpob/discretemathtranslator/Main.java
+++ src/main/java/ph/edu/dlsu/lbycpob/discretemathtranslator/Main.java
@@ -1,23 +1,40 @@
 package ph.edu.dlsu.lbycpob.discretemathtranslator;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
- * Entry point of the Discrete Math Translator application.
+ * Entry point of the application.
  *
- * <p>This class will be responsible for launching
- * the JavaFX application.</p>
+ * <p>Launches the JavaFX application and loads the main view
+ * from FXML.</p>
  */
-public class Main {
+public class Main extends Application {
 
     /**
-     * Starts the application.
+     * Launches the JavaFX runtime.
      *
-     * @param args command-line arguments
+     * @param args command-line arguments (unused)
      */
     public static void main(String[] args) {
+        launch(args);
+    }
 
-        // TODO:
-        // Launch the JavaFX application.
-
+    /**
+     * Loads the FXML view and displays the primary stage.
+     *
+     * @param primaryStage the primary window provided by JavaFX
+     * @throws Exception if the FXML file cannot be loaded
+     */
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainView.fxml"));
+        primaryStage.setTitle("Discrete Math Translator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
 }
