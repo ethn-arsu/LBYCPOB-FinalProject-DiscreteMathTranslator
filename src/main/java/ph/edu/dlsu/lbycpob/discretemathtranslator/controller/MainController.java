166d2aa97b4ff9652e501cecd5b02643837253d9
Zamuel Robes <zamuel_robes@dlsu.edu.ph>
2026-08-16 17:42:50 +0800

    Implementing the FXML in the controller

--- src/main/java/ph/edu/dlsu/lbycpob/discretemathtranslator/controller/MainController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/discretemathtranslator/controller/MainController.java
@@ -1,9 +1,15 @@
 package ph.edu.dlsu.lbycpob.discretemathtranslator.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 import ph.edu.dlsu.lbycpob.discretemathtranslator.engine.TranslatorEngine;
 import ph.edu.dlsu.lbycpob.discretemathtranslator.model.Expression;
 import ph.edu.dlsu.lbycpob.discretemathtranslator.model.TranslationResult;
-
+import java.util.Map;
 /**
  * Acts as the bridge between the JavaFX view and the
  * translation engine.
@@ -18,6 +24,30 @@ public class MainController {
      * The engine responsible for performing the actual translation.
      */
     private final TranslatorEngine translatorEngine;
+
+    /**
+     * Input field where the English statement is input.
+     */
+    @FXML
+    private TextField inputField;
+
+    /**
+     * Text area to display the translated result.
+     */
+    @FXML
+    private TextArea translationLabel;
+
+    /**
+     * Text area displaying the explanation for the translation.
+     */
+    @FXML
+    private TextArea explanationLabel;
+
+    /**
+     * Label displaying the translation status
+     */
+    @FXML
+    private Label statusLabel;
 
     /**
      * Initializes the controller with a new TranslatorEngine instance.
